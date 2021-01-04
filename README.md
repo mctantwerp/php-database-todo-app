cf5849aaf040fbbf49a483ebbd7fab2ba41d12c6
Alessandro Aussems <alessandro.aussems@meteor.be>
2021-01-04 16:01:30 +0100

    WIP

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # php-database-todo-app
-A simple TODO App written in PHP that uses a MySQL database
+A simple TODO App written in PHP that uses a MySQL database.
+
+Probeer de opdracht eerst zelf eens voor je naar de code gaat kijken.
+
+## Opdracht
+
+- We hebben een overzichtspagina van alle todo's.
+- Onderaan de pagina is er een formulier om een nieuwe todo toe te voegen.
+- Naast elke todo staat een knopje om het TODO-item te verwijderen
+
+### Resultaat
+
+![](./result.gif)
---
+++ app.php
@@ -0,0 +1,56 @@
+<?php
+try {
+    $connection = new PDO('mysql:host=localhost;dbname=todo_app','root','root');
+} catch (Exception $exception){
+    echo $exception->getMessage();
+    die;
+}
+//Query to get all todos
+$selectStatement = $connection->prepare('SELECT * FROM todos');
+$selectStatement->setFetchMode(PDO::FETCH_ASSOC);
+$selectStatement->execute();
+$todos = $selectStatement->fetchAll();
+//Form to get user input and add it to an insert query
+if($_SERVER['REQUEST_METHOD'] === 'POST'){
+    if(isset($_POST["title"])){
+        $insertStatement = $connection->prepare('INSERT INTO todos (title) VALUES (:title)');
+        $insertStatement->bindParam('title',$_POST["title"]);
+        $insertStatement->execute();
+    }
+    if(isset($_POST["id_to_delete"])){
+        $deleteStatement = $connection->prepare('DELETE FROM todos WHERE id = :id');
+        $deleteStatement->bindParam('id',$_POST['id_to_delete']);
+        $deleteStatement->execute();
+    }
+
+    header('Location: app.php');
+    die;
+}
+
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>ToDo</title>
+</head>
+<body>
+<h1>ToDo:</h1>
+<?php foreach ($todos as $todo): ?>
+    <li>
+        <?=$todo["title"] ?>
+        <form action="app.php" method="post">
+            <input type="hidden" name="id_to_delete" value="<?= $todo['id'] ?>">
+            <button>Delete</button>
+        </form>
+    </li>
+<?php endforeach; ?>
+<form action="app.php" method="post">
+    <input type="text" name="title">
+    <button>Add!</button>
+</form>
+</body>
+</html>
